a05097e0f010595da4e14f93f05c16f0d45a4e99
Vidip Ghosh <91741581+ghoshvidip26@users.noreply.github.com>
2025-02-27 17:29:14 +0530

    chore: Remove unnecessary routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { WalletProvider } from "./WalletContext";
 import Dashboard from "./components/Dashboard";
 import SendAmount from "./components/SendAmount";
 import Disperse from "./components/Disperse";
-import SwapFields from "./components/SwapFields";
 
 function App() {
   return (
@@ -12,9 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />}>
             <Route index element={<h1 className="text-2xl">Welcome to the Dashboard</h1>} />
-            <Route path="send" element={<SendAmount />} />
             <Route path="disperse" element={<Disperse />} />
-            <Route path="swap" element={<SwapFields />} />
           </Route>
         </Routes>
       </BrowserRouter>
